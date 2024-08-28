00a2c07228797f782aac986dfc7fb80e5b3b2b30
Serhan Khan <khanboy0689@gmail.com>
2024-08-29 00:46:15 +0300

    first commit

---
+++ GettingStartedWithTCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GettingStartedWithTCA
+//
+//  Created by Serhan Khan on 28/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GettingStartedWithTCA/GettingStartedWithTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GettingStartedWithTCAApp.swift
+//  GettingStartedWithTCA
+//
+//  Created by Serhan Khan on 28/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct GettingStartedWithTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

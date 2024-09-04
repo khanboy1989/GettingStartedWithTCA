bc0bd7957381f8f28a946b7c2e0e3534a3518e15
Serhan Khan <khanboy0689@gmail.com>
2024-09-04 23:05:59 +0300

    impl: First SideEffect to make a request

--- GettingStartedWithTCA/CounterFeature.swift
+++ GettingStartedWithTCA/CounterFeature.swift
@@ -11,14 +11,20 @@ import SwiftUI
 // Reducer that keeps state and the actions
 @Reducer
 struct CounterFeature {
+    @ObservableState
     struct State: Equatable {
         var count = 0
+        var isLoading = false
+        var fact: String?
     }
     
     enum Action {
         case decrementButtonTapped
         case incrementButtonTapped
         case resetButtonTapped
+        case factButtonTapped
+        case factResponse(String)
+        
     }
     
     var body: some ReducerOf<Self> {
@@ -26,12 +32,33 @@ struct CounterFeature {
             switch action {
             case .decrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
                 return .none
             case .incrementButtonTapped:
                 state.count += 1
+                state.fact = nil
                 return .none
             case .resetButtonTapped:
                 state.count = 0
+                state.fact = nil
+                return .none
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+                return .run {/*Captures the count's current value before triggering the event*/ [count = state.count] send in
+                    // ✅ Do async work in here, and send actions back into the system.
+                    // 🛑 Mutable capture of 'inout' parameter 'state' is not allowed in
+                    //    concurrently-executing code
+                    // 🛑 'async' call in a function that does not support concurrency
+                    // 🛑 Errors thrown from here are not handled
+                    let (data, _) = try await URLSession.shared
+                        .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(fact))
+                }
+            case let .factResponse(fact):
+                state.fact = fact
+                state.isLoading = false
                 return .none
             }
         }
@@ -88,6 +115,23 @@ struct CounterView: View {
                     .padding()
                     .background(Color.black.opacity(0.1))
                     .cornerRadius(10)
+                }
+                
+                Button("Fact") {
+                    store.send(.factButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+                
+                if store.isLoading {
+                    ProgressView()
+                } else if let fact = store.fact {
+                    Text(fact)
+                        .font(.largeTitle)
+                        .multilineTextAlignment(.center)
+                        .padding()
                 }
             }
         }

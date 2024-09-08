018a039affbd06063640be0a0b90122e2f89cbfd
Serhan Khan <khanboy0689@gmail.com>
2024-09-08 22:46:05 +0300

    impl: AddContactFeature & AddContactView

---
+++ GettingStartedWithTCA/AddContactView.swift
@@ -0,0 +1,20 @@
+//
+//  AddContactView.swift
+//  GettingStartedWithTCA
+//
+//  Created by Serhan Khan on 08/09/2024.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct AddContactView: View {
+    @Bindable var store: StoreOf<AddContactFeature>
+    
+    
+    var body: some View {
+        Form {
+            
+        }
+    }
+}

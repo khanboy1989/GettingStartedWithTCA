c9f4ed61cbbf71014387c4a5921f923fae1a2ea0
Serhan Khan <serhankhan0@gmail.com>
2024-09-19 11:32:02 +0300

    impl: Tracking Tab Changes in AppFeature

--- GettingStartedWithTCA/Features/AppFeature.swift
+++ GettingStartedWithTCA/Features/AppFeature.swift
@@ -14,18 +14,20 @@ struct AppView: View {
     let store: StoreOf<AppFeature>
     
     var body: some View {
-        TabView(selection: store.binding(
-            get: { $0 }, send: AppFeature.Action.setActiveTab)) {
-            CounterView(store: store.scope(state: \.tab1, action: \.tab1))
-                .tabItem {
-                    Text("Counter 1")
-                }.tag(AppFeature.State.Tab.tab1)
-            
-            CounterView(store: store.scope(state: \.tab2, action: \.tab2))
-                .tabItem {
-                    Text("Counter 2")
-                }.tag(AppFeature.State.Tab.tab2)
+        WithViewStore(self.store, observe: { $0 }) {viewStore in
+            TabView(selection: viewStore.binding(get: \.selectedTab, send: AppFeature.Action.selectedTab)) {
+                CounterView(store: store.scope(state: \.tab1, action: \.tab1))
+                    .tabItem {
+                        Text("Counter 1")
+                    }.tag(AppFeature.Tab.tab1)
+                
+                CounterView(store: store.scope(state: \.tab2, action: \.tab2))
+                    .tabItem {
+                        Text("Counter 2")
+                    }.tag(AppFeature.Tab.tab2)
+            }
         }
+        
     }
 }
 
@@ -38,25 +40,24 @@ struct AppView: View {
 @Reducer
 struct AppFeature {
     
+    enum Tab {
+        case tab1
+        case tab2
+    }
+    
     // Composing State
     @ObservableState
     struct State: Equatable {
         var tab1 = CounterFeature.State()
         var tab2 = CounterFeature.State()
-        // Property to track the active tab
-        var activeTab: Tab = .tab1
-        
-        enum Tab {
-            case tab1
-            case tab2
-        }
+        var selectedTab: AppFeature.Tab = .tab1
     }
     
     // Composing Action
     enum Action {
         case tab1(CounterFeature.Action)
         case tab2(CounterFeature.Action)
-        case setActiveTab(State.Tab)
+        case selectedTab(AppFeature.Tab)
     }
     
     var body: some ReducerOf<Self> {
@@ -77,27 +78,22 @@ struct AppFeature {
         
         Reduce { state, action in
             // Core logic of the app feature
-            
             switch action {
             case .tab1:
-                state.activeTab = .tab1
+                state.selectedTab = .tab1
                 print("Tab 1 is active")
+                return .none
                 
             case .tab2:
-                state.activeTab = .tab2
+                state.selectedTab = .tab2
                 print("Tab 2 is active")
+                return .none
                 
-            case .setActiveTab(let tab):
-                state.activeTab = tab
+            case .selectedTab(let tab):
+                state.selectedTab = tab
+                print("Active tab: \(tab)")
+                return .none
             }
-            
-            //            switch state {
-            //            case .tab1:
-            //                print("Tab 1")
-            //            case .tab2:
-            //                print("Tab 2")
-            //            }
-            return .none
         }
     }
 }
